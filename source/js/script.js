31e95b61d1c6e973bad905f11188b7840eb281d5
Алексей Агафонов <dr_agafony@mail.ru>
2019-04-29 17:28:55 +0300

    Делает стилизацию модального окна для всех раскладок (мобилка, планшет, десктоп).

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,26 @@
 const modalBasket = document.querySelector(`.modal-basket`);
 const featuredButton = document.querySelector(`.featured__button`);
-const addButton = document.querySelector(`.modal-basket__add`);
+const addButton = document.querySelector(`.modal-basket__button`);
 const modalOverlay = document.querySelector(`.modal-baskte__overlay`);
 const modalMain = document.querySelector(`.modal-basket__main`);
 
 featuredButton.addEventListener(`click`, (evt) => {
   evt.preventDefault();
   modalBasket.classList.remove("invisible-block");
-  modalOverlay.style.width = `320px`;
+  modalBasket.style.width = document.documentElement.scrollWidth + `px`;
+  modalOverlay.style.width = document.documentElement.scrollWidth + `px`;
   modalOverlay.style.height = document.documentElement.scrollHeight + `px`;
 
   modalMain.style.top = (window.pageYOffset + (document.documentElement.clientHeight / 2) - 168) + `px`;
-  modalMain.style.left = (window.pageXOffset + (document.documentElement.clientWidth / 2) - 160) + `px`;
+
+  if (document.documentElement.clientWidth < 768) {
+    modalMain.style.left = (window.pageXOffset + (document.documentElement.clientWidth / 2) - 160) + `px`;
+  } else {
+    modalMain.style.left = (window.pageXOffset + (document.documentElement.clientWidth / 2) - 283) + `px`;
+  }
 });
 
-addButton.addEventListener(`click`, () => {
+addButton.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
   modalBasket.classList.add("invisible-block");
 });
